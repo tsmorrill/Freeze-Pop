800dfcc74670e1d2c8b1ad47d80020f4f0337fa0
Thomas Morrill <t.morrill@adfa.edu.au>
2021-08-12 20:45:50 -0400

    Update dither-melody.py

--- code/dither-melody.py
+++ code/dither-melody.py
@@ -5,12 +5,13 @@ import random
 import string
 import sys
 
+import numpy as np
+
+from dither import dither_1D
 from heightmap import diamond_square
 from midiutil.MidiFile import MIDIFile
 
-scale = [60, 62, 64, 66, 67, 69, 71,
-         72, 74, 76, 78, 79, 81, 83,
-         84]
+scale = [60, 62, 67]
 
 if len(sys.argv) > 1:
     seed = str(sys.argv[1])
@@ -19,51 +20,52 @@ else:
     seed = ''.join(random.choice(chars) for i in range(8))
 
 iter = 4
-length = 2**iter
-smoothing = 1.2
+smoothing = 0
 
-stress_map = diamond_square(iter, smoothing, seed + "stress_map")
-# trim to 2^iter square and flatten
-stress_list = []
-for i in range(length // 2):
-    stress_list.extend(stress_map[i][:length])
-    stress_list.extend(stress_map[i + length//2][:length])
+stress_init = np.array([[1, 0],
+                 [1, 0]])
+stress_map = diamond_square(iter, smoothing, seed + "stress_map", stress_init)
+# delete bottom row
+stress_map = np.delete(stress_map, -1, 0)
+# delete right column
+stress_map = np.delete(stress_map, -1, 1)
+stress_map = stress_map.flatten()
 
-smoothing = 1.4
+smoothing = 0.2
 
-pitch_map = diamond_square(iter, smoothing, seed + "pitch_map")
-# trim to 2^iter square and flatten
-pitch_list = []
-for i in range(length // 2):
-    pitch_list.extend(pitch_map[i][:length])
-    pitch_list.extend(pitch_map[i +  length//2][:length])
+pitch_init = np.array([[0, 0.5],
+                       [0.5, 1]])
+pitch_map = diamond_square(iter, smoothing, seed + "pitch_map", pitch_init)
+# delete bottom row
+pitch_map = np.delete(pitch_map, -1, 0)
+# delete right column
+pitch_map = np.delete(pitch_map, -1, 1)
+pitch_map = pitch_map.flatten()
 
 # dither
 threshold = []
-format_string = '0' + str(iter) + 'b'
-for i in range(length)[::-1]:
-    threshold.append((int(format(i, format_string)[::-1], 2)+0.5)/length)
-rhythm = []
-for i in range(length**2):
-    rhythm.append(stress_list[i] <= threshold[i % length])
+threshold = dither_1D(2*iter)
+# comma required to denote 1-tuple
+rhythm = [threshold[i % len(threshold)] < value
+          for (i,), value in np.ndenumerate(stress_map)]
 
 output_file = MIDIFile(1)
 track = 0
 time = 0
 channel = 0
 duration = 1/4
-track_name = "melody " + seed + " {} bar".format(len(stress_list) // 16)
-if (len(stress_list) // 16) > 1:
+track_name = "melody " + seed + " {} bar".format(len(rhythm) // 16)
+if (len(rhythm) // 16) > 1:
     track_name += "s"
 output_file.addTrackName(track, time, track_name)
 for index, play_note in enumerate(rhythm):
     if play_note:
-        pitch_index = math.floor(len(scale)*pitch_list[index])
+        pitch_index = math.floor(len(scale)*pitch_map[index])
         pitch_index = min(pitch_index, len(scale)-1)
         pitch = scale[pitch_index]
         time = index/4
         # no zero velocity notes
-        volume = math.ceil(stress_list[index]*127)
+        volume = math.ceil(stress_map[index]*127)
         volume = max(volume, 1)
         output_file.addNote(track, channel, pitch, time, duration, volume)
 
