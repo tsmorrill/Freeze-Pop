b3bf955d4e605690c36948200883705942d7e44e
Thomas Morrill <morrill.math@gmail.com>
2022-04-30 15:01:34 -0400

    Delete heightmap-png.py

--- heightmap-png.py
+++
@@ -1,6 +0,0 @@
-from midigen import heightmap
-
-map = heightmap.diamond_square(8, 0.5, "Math Center", None)
-map = heightmap.heightmap_normalize(map)
-print("Map in memory")
-heightmap.heightmap_to_png(map, "mathcenter")
