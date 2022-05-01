471dabc04ae0a22dad6d3c99c4e17abe7166edcb
Thomas Morrill <morrill.math@gmail.com>
2022-05-01 12:52:36 -0400

    check for size of k

--- midigen/bresenham.py
+++ midigen/bresenham.py
@@ -1,6 +1,8 @@
 def euclid(i, k, n):
     """Determine whether a pulse occurs on beat i in the (k,n) Euclidean rhythm.
     """
+    if k > n:
+        raise Exception("k cannot exceed n.")
     rollover = (k*i % n) > (k*(i+1) % n)
     return(rollover)
 
@@ -12,4 +14,4 @@ def Bresenham(k, n):
 
 
 if __name__ == "__main__":
-    print(Bresenham(7, 16))
+    print(Bresenham(11, 7))
