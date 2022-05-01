7d01fcf78e726c8889d78e6e991b56ebbe4589ae
Thomas Morrill <morrill.math@gmail.com>
2022-05-01 12:40:05 -0400

    shorten Euclidean algorithm

--- midigen/bresenham.py
+++ midigen/bresenham.py
@@ -1,13 +1,15 @@
-def Bresenham(k,n):
-    """Compute Euclidean rhythm of k pulses and length n."""
-    list = []
-    cumDiff = -k
+def euclid(t, k, n):
+    """Determine whether a pulse occurs on beat t in the (k,n) Euclidean rhythm.
+    """
+    rollover = (k*t % n) > (k*(t+1) % n)
+    return(rollover)
 
-    for x in range(n):
-        if cumDiff < 0:
-            cumDiff += n
-            list.append(True)
-        else:
-            list.append(False)
-        cumDiff -= k
-    return(list)
+
+def Bresenham(k, n):
+    """Return Euclidean rhythm of k pulses and length n."""
+    list = [euclid(i - k, k, n) for i in range(n)]      # shift by -k to ensure
+    return(list)                                        # list[0] == True
+
+
+if __name__ == "__main__":
+    print(Bresenham(7, 16))
