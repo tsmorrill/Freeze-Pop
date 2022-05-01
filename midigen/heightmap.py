4fad4cb879053d18d50aab24a8d66af667cc2ff0
Thomas Morrill <morrill.math@gmail.com>
2022-05-01 13:17:02 -0400

    remove png code

--- midigen/heightmap.py
+++ midigen/heightmap.py
@@ -1,10 +1,11 @@
 import numpy as np
 import random
 
+
 def heightmap_1D(iter, smoothing, seed, init):
     """Create 2^iter + 1 linear heightmap via midpoint displacement.
     """
-    if init == None:
+    if init is None:
         random.seed(seed + "init")
         heightmap = np.array([random.random(), random.random()])
     else:
@@ -16,7 +17,7 @@ def heightmap_1D(iter, smoothing, seed, init):
         for j in range(2**i):
             temp_list.append(heightmap[j])
             temp_list.append((heightmap[j]+heightmap[j+1])/2
-                             + random.uniform(-1,1)*2**(-smoothing*(i+1)))
+                             + random.uniform(-1, 1)*2**(-smoothing*(i+1)))
         temp_list.append(heightmap[-1])
         heightmap = np.array(temp_list)
 
@@ -25,10 +26,11 @@ def heightmap_1D(iter, smoothing, seed, init):
     heightmap /= heightmap.max()
     return(heightmap)
 
+
 def diamond_square(iter, smoothing, seed, init):
     """Create 2^iter + 1 square heightmap via diamond square algorithm.
     """
-    if init == None:
+    if init is None:
         random.seed(seed + "init")
         heightmap = np.array([[random.random(), random.random()],
                               [random.random(), random.random()]])
@@ -40,12 +42,12 @@ def diamond_square(iter, smoothing, seed, init):
         rows, cols = heightmap.shape
         temp_map = np.zeros((2*rows - 1, 2*cols - 1))
         jitter_diamond = 2**(-smoothing*(2*n + 1))
-        jitter_square  = 2**(-smoothing*(2*n + 2))
+        jitter_square = 2**(-smoothing*(2*n + 2))
         for (i, j), value_ij in np.ndenumerate(heightmap):
             north_exists = i > 0
             south_exists = i < rows - 1
-            west_exists  = j > 0
-            east_exists  = j < cols - 1
+            west_exists = j > 0
+            east_exists = j < cols - 1
 
             temp_map[2*i, 2*j] = heightmap[i, j]
 
@@ -53,37 +55,37 @@ def diamond_square(iter, smoothing, seed, init):
                 diamond_center = (heightmap[i, j] + heightmap[i, j+1]
                                   + heightmap[i+1, j] + heightmap[i+1, j+1])
                 diamond_center /= 4
-                diamond_center += random.uniform(-1,1)*jitter_diamond
+                diamond_center += random.uniform(-1, 1)*jitter_diamond
                 temp_map[2*i + 1, 2*j + 1] = diamond_center
 
                 square_top = (heightmap[i, j] + heightmap[i, j+1]
-                             + diamond_center)
+                              + diamond_center)
                 if north_exists:
                     square_top += temp_map[2*i - 1, 2*j + 1]
                     square_top /= 4
                 else:
                     square_top /= 3
-                square_top += random.uniform(-1,1)*jitter_square
+                square_top += random.uniform(-1, 1)*jitter_square
                 temp_map[2*i, 2*j + 1] = square_top
 
                 square_left = (heightmap[i, j] + heightmap[i+1, j]
-                              + diamond_center)
+                               + diamond_center)
                 if west_exists:
                     square_left += temp_map[2*i + 1, 2*j - 1]
                     square_left /= 4
                 else:
                     square_left /= 3
-                square_left += random.uniform(-1,1)*jitter_square
+                square_left += random.uniform(-1, 1)*jitter_square
                 temp_map[2*i + 1, 2*j] = square_left
             elif east_exists and not south_exists:
                 square_top = (heightmap[i, j] + heightmap[i, j+1]
-                             + temp_map[2*i - 1, 2*j + 1])/3
-                square_top += random.uniform(-1,1)*jitter_square
+                              + temp_map[2*i - 1, 2*j + 1])/3
+                square_top += random.uniform(-1, 1)*jitter_square
                 temp_map[2*i, 2*j + 1] = square_top
             elif not east_exists and south_exists:
                 square_left = (heightmap[i, j] + heightmap[i+1, j]
-                             + temp_map[2*i + 1, 2*j - 1])/3
-                square_left += random.uniform(-1,1)*jitter_square
+                               + temp_map[2*i + 1, 2*j - 1])/3
+                square_left += random.uniform(-1, 1)*jitter_square
                 temp_map[2*i + 1, 2*j] = square_left
         heightmap = temp_map
 
@@ -98,14 +100,16 @@ def diamond_square(iter, smoothing, seed, init):
     heightmap /= heightmap.max()
     return(heightmap)
 
+
 def map_interp(heightmap):
     rows, cols = heightmap.shape
     temp_map = np.zeros((rows - 1, cols - 1))
     for (i, j), value_ij in np.ndenumerate(temp_map):
         average = (heightmap[i, j] + heightmap[i, j+1]
-                  + heightmap[i+1, j] + heightmap[i+1, j+1])/4
-        temp_map[i, j] =  average
+                   + heightmap[i+1, j] + heightmap[i+1, j+1])/4
+        temp_map[i, j] = average
     return(temp_map)
+
 
 def trim_and_flatten(heightmap, rows=1, cols=1):
     for i in range(rows):
@@ -115,24 +119,28 @@ def trim_and_flatten(heightmap, rows=1, cols=1):
     heightmap = heightmap.flatten()
     return(heightmap)
 
+
 def entrywise_product(heightmap0, heightmap1, normalize=True):
     output = np.zeros(heightmap0.shape)
     for (i, j), value in np.ndenumerate(heightmap0):
-        output[i,j] = heightmap0[i,j]*heightmap1[i,j]
+        output[i, j] = heightmap0[i, j]*heightmap1[i, j]
     if normalize:
         output = heightmap_normalize(output)
     return(output)
+
 
 def heightmap_normalize(heightmap):
     heightmap -= heightmap.min()
     heightmap /= heightmap.max()
     return(heightmap)
 
+
 def mean_normalize(heightmap, new_mean):
     size = heightmap.shape
     old_mean = np.sum(heightmap.reshape(1, -1))/size
     heightmap = heightmap*new_mean/old_mean
     return(heightmap)
+
 
 def heightmap_radar_list(heightmap, r_step, theta_step,
                          init_angle=0, sweep=np.pi):
@@ -147,12 +155,13 @@ def heightmap_radar_list(heightmap, r_step, theta_step,
             # correct for orientation
             i, j = int(np.floor(y + N/2)), int(np.floor(x + N/2))
             u, v = y + N/2 - i, x + N/2 - j
-            A, B = heightmap[  i, j], heightmap[  i, j+1]
+            A, B = heightmap[i, j], heightmap[i, j+1]
             C, D = heightmap[i+1, j], heightmap[i+1, j+1]
             interp = (A*(1-v) + B*v)*(1-u) + (C*(1-v) + D*v)*u
             list.append(interp)
     list = np.array(list)
     return(list)
+
 
 def erode(heightmap, seed, iter):
     rows, cols = heightmap.shape
@@ -165,8 +174,8 @@ def erode(heightmap, seed, iter):
         while droplet_volume > 0:
             north_exists = i > 0
             south_exists = i < rows - 1
-            west_exists  = j > 0
-            east_exists  = j < cols - 1
+            west_exists = j > 0
+            east_exists = j < cols - 1
 
             current_min = heightmap[i, j]
             choices = [(0, 0)]
@@ -218,35 +227,3 @@ def erode(heightmap, seed, iter):
     heightmap -= heightmap.min()
     heightmap /= heightmap.max()
     return(heightmap)
-
-def heightmap_to_png(heightmap, filename):
-    rows, cols = heightmap.shape
-
-    x = [i for i in range(cols)]
-    # correct for orientation
-    y = [-i for i in range(rows)]
-    # prepare for plot_surface
-    x, y = np.meshgrid(x, y)
-    z = heightmap
-
-    px = 1/plt.rcParams['figure.dpi']  # pixel in inches
-    fig = plt.figure(figsize = (800*px, 800*px))
-    ax = plt.axes(projection = '3d')
-
-    my_cmap = plt.get_cmap('cool')
-
-    stride = 1
-    surf = ax.plot_surface(x, y, z, cmap = my_cmap, rstride=stride, cstride=stride, antialiased=False)
-
-    ax.view_init(elev=20, azim=290)
-    ax.set_title(filename)
-
-    plt.gca().axes.get_xaxis().set_ticks([])
-    plt.gca().axes.get_yaxis().set_ticks([])
-
-    plt.xlabel('X')
-    plt.ylabel('Y')
-
-    filename += '.png'
-    plt.savefig(filename)
-    plt.close('all')
