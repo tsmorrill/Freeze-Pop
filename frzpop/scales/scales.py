f30d7d81f031993371fc98400fa14f84a3557b0b
Thomas Morrill <morrill.math@gmail.com>
2022-06-28 19:03:51 -0400

    add scales submodule

---
+++ frzpop/scales/scales.py
@@ -0,0 +1,37 @@
+notes = {"C":   0,
+         "Cs":  1, "Db":  1,
+         "D":   2,
+         "Ds":  3, "Eb":  3,
+         "E":   4,
+         "F":   5,
+         "Fs":  6, "Gb":  6,
+         "G":   7,
+         "Gs":  8, "Ab":  8,
+         "A":   9,
+         "As": 10, "Bb": 10,
+         "B":  11, }
+
+intervals = {"major": [0, 2, 4, 5, 7, 9, 11],
+             "minor": [0, 2, 3, 5, 7, 8, 10]}
+
+
+def make_scale(root, type):
+    root %= 12
+    degrees = intervals[type]
+    degrees = [root + degree for degree in degrees]
+    all_octaves = [n for n in range(128) if n % 12 in degrees]
+    return all_octaves
+
+
+def major(root):
+    return make_scale(root, "major")
+
+
+def minor(root):
+    return make_scale(root, "minor")
+
+
+if __name__ == "__main__":
+    scale = minor(60)
+    for i in range(0, len(scale), 7):
+        print(scale[i:i+8])
