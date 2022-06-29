d39106f0f16fe19a8e6136faa9abc9ab11233278
Thomas Morrill <morrill.math@gmail.com>
2022-06-29 14:35:35 -0400

    add chord submodule

---
+++ frzpop/chords/chords.py
@@ -0,0 +1,24 @@
+intervals = {"major":      [0, 4, 7],
+             "minor":      [0, 3, 7]}
+
+
+def make_chord(root, type):
+    root %= 12
+    degrees = intervals[type]
+    degrees = [(root + degree) % 12 for degree in degrees]
+    all_octaves = [n for n in range(128) if n % 12 in degrees]
+    return all_octaves
+
+
+def major(root):
+    return make_chord(root, "major")
+
+
+def minor(root):
+    return make_chord(root, "minor")
+
+
+if __name__ == "__main__":
+    chord = minor(11)
+    for i in range(0, len(chord), 3):
+        print(chord[i:i+3])
