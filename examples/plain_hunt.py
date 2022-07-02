f8b49dc1df374012baa11b6f4cac2c5302fbeb10
Thomas Morrill <morrill.math@gmail.com>
2022-07-02 09:39:28 -0400

    change octave

--- examples/plain_hunt.py
+++ examples/plain_hunt.py
@@ -1,18 +1,18 @@
 import frzpop
-from frzpop.notes import C3, D3, E3, F3, G3, A3, B3, C4
+from frzpop.notes import C4, D4, E4, F4, G4, A4, B4, C5
 from frzpop.dynamics import f
 from frzpop.phrases import make_plain_hunt
 
 eighth = frzpop.freezers.make_freezer(note_len=1/2)
 
-init_phrase = [[C4, f, eighth],
-               [B3, f, eighth],
-               [A3, f, eighth],
-               [G3, f, eighth],
-               [F3, f, eighth],
-               [E3, f, eighth],
-               [D3, f, eighth],
-               [C3, f, eighth]]
+init_phrase = [[C5, f, eighth],
+               [B4, f, eighth],
+               [A4, f, eighth],
+               [G4, f, eighth],
+               [F4, f, eighth],
+               [E4, f, eighth],
+               [D4, f, eighth],
+               [C4, f, eighth]]
 phrase_generator = make_plain_hunt(init_phrase)
 
 section = [phrase_generator for _ in range(16)]
