c094ca09fe76395869bcdd098c3752d0cd9d0351
Thomas Morrill <morrill.math@gmail.com>
2022-07-03 10:59:10 -0400

    let the linter complain

--- frzpop/__init__.py
+++ frzpop/__init__.py
@@ -1,5 +1,2 @@
 from .freezers import freeze_phrase, freeze_section, freeze_song, freeze_track
 from .freezers import make_cube, make_freezer
-
-__all__ = ['freeze_phrase', 'freeze_section', 'freeze_song', 'freeze_track',
-           'make_cube', 'make_freezer']
