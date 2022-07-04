63b9bfaa1e677138abb0f8b0aca05836a0667db3
Thomas Morrill <morrill.math@gmail.com>
2022-07-03 21:37:23 -0400

    tweak naming

--- examples/strum.py
+++ examples/strum.py
@@ -5,17 +5,17 @@ from frzpop.generators import sweep
 from frzpop.quantize import quantizer
 
 A = 69
-Astrum = quantizer(major(A))
+A_maj = quantizer(major(A))
 
 sweep1 = sweep(40, 70, 16)
 sweep2 = sweep(45, 80, 16)
 sweep3 = sweep(35, 75, 16)
 sweep4 = sweep(30, 85, 16)
 
-bar1 = [[Astrum(sweep1()), f, None] for _ in range(16)]
-bar2 = [[Astrum(sweep2()), f, None] for _ in range(16)]
-bar3 = [[Astrum(sweep3()), f, None] for _ in range(16)]
-bar4 = [[Astrum(sweep4()), f, None] for _ in range(16)]
+bar1 = [[A_maj(sweep1()), f, None] for _ in range(16)]
+bar2 = [[A_maj(sweep2()), f, None] for _ in range(16)]
+bar3 = [[A_maj(sweep3()), f, None] for _ in range(16)]
+bar4 = [[A_maj(sweep4()), f, None] for _ in range(16)]
 
 section = [bar1, bar2, bar3, bar4]
 
