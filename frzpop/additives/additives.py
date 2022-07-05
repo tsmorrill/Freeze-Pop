ce8123eade85aa5ce3fa1babea4524fe88506400
Thomas Morrill <morrill.math@gmail.com>
2022-07-05 07:17:37 -0400

    add rng state machine

--- frzpop/additives/additives.py
+++ frzpop/additives/additives.py
@@ -1,3 +1,6 @@
+from random import Random
+
+
 def water():
     print("Refreshing!")
 
@@ -12,3 +15,19 @@ def state_machine(gen):
             return next(generator)
         return(func)
     return wrapper
+
+
+@state_machine
+def rng(seed=None):
+    rng = Random(seed)
+
+    while True:
+        yield rng.random()
+
+
+if __name__ == "__main__":
+    rng1 = rng(1019)
+    rng2 = rng(1019)
+
+    print(rng1(), rng1(), rng1(), rng1())
+    print(rng2(), rng2(), rng2(), rng2())
