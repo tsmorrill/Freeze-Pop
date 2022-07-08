61225be86e33bb16c3a9ff71469a215087168700
Thomas Morrill <morrill.math@gmail.com>
2022-07-07 20:42:58 -0400

    add type hinting

--- frzpop/chords.py
+++ frzpop/chords.py
@@ -3,37 +3,37 @@ from frzpop import additives
 gamut = additives.gamut
 
 
-def major(root):
+def major(root: int) -> list:
     intervals = [0, 4, 3]
     return gamut(root, intervals)
 
 
-def minor(root):
+def minor(root: int) -> list:
     intervals = [0, 3, 4]
     return gamut(root, intervals)
 
 
-def dim(root):
+def dim(root: int) -> list:
     intervals = [0, 3, 3]
     return gamut(root, intervals)
 
 
-def aug(root):
+def aug(root: int) -> list:
     intervals = [0, 5, 2]
     return gamut(root, intervals)
 
 
-def maj7(root):
+def maj7(root: int) -> list:
     intervals = [0, 4, 3, 4]
     return gamut(root, intervals)
 
 
-def dom7(root):
+def dom7(root: int) -> list:
     intervals = [0, 4, 3, 3]
     return gamut(root, intervals)
 
 
-def min7(root):
+def min7(root: int) -> list:
     intervals = [0, 3, 4, 3]
     return gamut(root, intervals)
 
@@ -41,4 +41,4 @@ def min7(root):
 if __name__ == "__main__":
     c7 = min7(60)
     for i in range(0, len(c7), 4):
-        print(c7[i : i + 4])
+        print(c7[i: i + 4])
