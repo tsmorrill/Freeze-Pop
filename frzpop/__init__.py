96f48f7c37ca186883c038652e0c28a5a0ada87b
Thomas Morrill <morrill.math@gmail.com>
2022-07-17 14:17:33 -0400

    add freeze_foo functions to namespace

--- frzpop/__init__.py
+++ frzpop/__init__.py
@@ -10,3 +10,8 @@ from frzpop import (
     scales,
     shuffle,
 )
+
+freeze_phrase = freezers.freeze_phrase
+freeze_section = freezers.freeze_section
+freeze_song = freezers.freeze_song
+freeze_track = freezers.freeze_track
